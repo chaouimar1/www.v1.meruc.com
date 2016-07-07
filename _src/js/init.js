6e53e21fce24f901dff42c85422cce59fc70cec9
mer1 <chaoui.mar2@gmail.com>
2016-07-07 17:06:12 +0000

    typed slider and ubuntu font

--- _src/js/init.js
+++ _src/js/init.js
@@ -12,7 +12,7 @@
   });
 
   $('.scollmenu').click(function(){
-      $('html, body').animate({
+      $('html, body').stop().animate({
           scrollTop: $( $(this).attr('href') ).offset().top
       }, 1000);
       return false;
@@ -26,3 +26,23 @@ function tabselect(type){
   $('.micards .card').fadeTo( "fast", 0.5 );
   $('.micards .'+type).fadeTo( "fast", 1 );
 };
+
+$(function(){
+    $(".elements").typed({
+      strings: [
+              "^300un <span class=\"deep-purple\"><u>Développeur web</u></span>",
+              "^300un <span><u>Frontend'er</u></span>",
+              "^300un <span><u>Débutant <span class=\"light-green darken-1\">Android</span></u></span>",
+              "^300un <span><u>Utilisateur <span class=\"orange ubufont\">Ubuntu</span></u></span>",
+              "^300<span class=\"white grey-text text-darken-4\"><u>&nbsp;Marwane CHAOUI&nbsp;</u></span>^15000"
+      ],
+      contentType: 'html',
+      loop: true,
+      cursorChar: "_",
+      typeSpeed: 60,
+      backSpeed: 10,
+      shuffle: false,
+      startDelay: 500,
+      callback: function() {/*alert('done !')*/},
+    });
+});
